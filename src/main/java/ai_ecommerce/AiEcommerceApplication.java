fe5bdce336a6cea05d8f5f49f90d23b2bfd47277
sr325 <san_hiske@hotmail.co.uk>
2025-03-16 14:41:03 +0000

    initial commit

---
+++ src/main/java/ai_ecommerce/AiEcommerceApplication.java
@@ -0,0 +1,13 @@
+package ai_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiEcommerceApplication.class, args);
+	}
+
+}
